93943e787fc6fc1a05739bc556abef7fe95ac901
Steven Von Haus <johndoe@example.com>
2018-10-20 10:47:01 +0200

    feat-1: create an endpoints which says hello

---
+++ src/main/java/be/henallux/gitdemo/HelloController.java
@@ -0,0 +1,13 @@
+package be.henallux.gitdemo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
